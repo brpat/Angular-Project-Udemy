2ac48a674e8f3b773defd5627303bb0448f87c9c
Brijesh Patel <brijeshpatel@Brijeshs-MacBook-Pro.local>
2022-10-08 18:35:41 -0400

    Added frontend and backend routing and edit page logic

--- src/app/posts/posts-create/posts-create.component.ts
+++ src/app/posts/posts-create/posts-create.component.ts
@@ -1,7 +1,10 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import {Post} from '../post.model';
 import { PostService } from '../posts.service';
+import { ActivatedRoute } from '@angular/router';
+import { ParamMap } from '@angular/router';
+import * as e from 'express';
 
 
 @Component({
@@ -9,20 +12,44 @@ import { PostService } from '../posts.service';
   templateUrl: './posts-create.component.html',
   styleUrls: ['./posts-create.component.css']
 })
-export class PostsCreateComponent {
+export class PostsCreateComponent implements OnInit{
   enteredValue = '';
   enteredTitle = '';
   postService: PostService;
-  constructor(postService:PostService) { 
+  private mode = 'edit';
+  private postId:string|null|undefined = '';
+  post:Post|null = null;
+  constructor(postService:PostService, public route: ActivatedRoute) { 
     this.postService = postService;
   }
 
-  onAddPost(form:NgForm){
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap:ParamMap) => {
+      if(paramMap.has('postId')){
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        this.postService.getPost(this.postId).subscribe(postData=>{
+          this.post = {id:postData._id, title:postData.title, content:postData.content};
+          console.log(this.post);
+        });
+      }else{
+        this.mode = 'create';
+        this.postId = null;
+      }
+    });
+  }
+
+  onSavePost(form:NgForm){
     //newPost = postInputRef.value;
     if (form.invalid){
       return;
     }
-    this.postService.addPost(form.value.title, form.value.content);
+
+    if(this.mode == 'create'){
+      this.postService.addPost(form.value.title, form.value.content);
+    } else{
+      this.postService.updatePost(this.postId, form.value.title, form.value.content);
+    }
     form.resetForm();
   }
 
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -33,6 +33,24 @@ export class PostService{
         return this.postsUpdated.asObservable();
     }
 
+    getPost(id:string|null){
+        return this.http.get<{_id:string, title:string, content:string}>("http://localhost:3000/api/posts/"+id);
+        //return {...this.posts.find(post => post.id === id)};
+    }
+
+    updatePost(id:string, title:string, content:string){
+        const post:Post = {id:id, title:title, content:content};
+        this.http.put("http://localhost:3000/api/posts/"+id, post)
+        .subscribe(response =>{
+            const updatedPosts = [...this.posts];
+            const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+            updatedPosts[oldPostIndex] = post;
+            this.posts = updatedPosts;
+            this.postsUpdated.next([...this.posts]);
+        });
+    }
+
+
     addPost(title:string, content:string){
         const post = {
             id:'null',
