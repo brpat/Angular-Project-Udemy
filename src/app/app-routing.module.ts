6e3a2dcfcf62dff1e75fd2073ce66bf9f16a1d60
Brijesh Patel <brijeshpatel@Brijeshs-MacBook-Pro.local>
2022-11-06 17:25:15 -0500

    Added sign up component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 import { PostsCreateComponent } from './posts/posts-create/posts-create.component';
 import { PostsListComponent } from './posts/posts-list/posts-list.component';
 
@@ -9,7 +10,8 @@ const routes: Routes = [
   { path: '', component: PostsListComponent },
   { path: 'create', component: PostsCreateComponent },
   { path: 'edit/:postId', component: PostsCreateComponent },
-  { path:'login', component: LoginComponent}
+  { path:'login', component: LoginComponent},
+  { path:'signup', component: SignupComponent}
 ];
 
 @NgModule({
